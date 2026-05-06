2dfc4797cc0de68e808cc09e45b5a4ade8e3879d
yucksservices <130457306+yucksservices@users.noreply.github.com>
2026-05-05 23:48:38 -0700

    server.js

--- server/server.js
+++ server/server.js
@@ -205,7 +205,13 @@ app.post("/api/order", upload.array("inspirationPhotos", 5), async (req, res) =>
     });
   }
 });
+const DIST_DIR = path.join(__dirname, "../dist");
 
+app.use(express.static(DIST_DIR));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(DIST_DIR, "index.html"));
+});
 app.listen(PORT, () => {
   console.log(`BakeWithLinaa backend running on http://localhost:${PORT}`);
 });
